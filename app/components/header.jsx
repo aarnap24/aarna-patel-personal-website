81178efa1182f7fdceac5fce5d0374311462607e
aarna <aarnapatel18@gmail.com>
2026-09-03 21:49:48 -0500

    layout updates

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,6 +1,11 @@
 export default function Header() {
   return (
     <div id="home">
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <h2>Welcome! My name is</h2>
       <div className="typewriter-wrapper">
         <h1 className="typewriter">AARNA PATEL</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="navbar">
         <ul>
           <li> <a href="#about">ABOUT</a> </li>
-          <li> <a href="/projects">PROJECTS</a> </li>
+          <li> <a href="#projects">PROJECTS</a> </li>
           <li> <a href="#contact">CONTACT</a> </li>
         </ul>
       </div>
@@ -20,15 +20,24 @@ export default function Home() {
       <br></br>
       <br></br>
       <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
       <div className="info-card" id="about">
         <div className="info-card-text">
-          <h2>ABOUT</h2>
-          <p>Hello! My name is Aarna Patel. I am currently an undergrad at 
-            the University of Illinois Chicago, majoring in computer science and design.
-            I am interested in web development and game design, and I am looking to gain
-            experience through internships. This website is my digital portfolio showcasing
-            my skills in CS and design. Happy exploring!
+          <h1>ABOUT ME</h1>
+          <p>
+            Hey! I'm Aarna, currently an undergrad at the University of Illinois Chicago, 
+            majoring in Computer Science + Design. I'm interested in UI/UX design, 
+            web development, and game design. I am always interested in opportunities to 
+            expand and apply my skillset! In my free time, I love reading and playing video 
+            games!
+          </p>
+          <br></br>
+          <p>  
+            This website is my digital portfolio showcasing my work in CS 
+            and design. Happy exploring!
           </p>
         </div>
         <img src="mepicnew.jpg" alt="A picture of me" width="400"/>
@@ -37,24 +46,27 @@ export default function Home() {
       <br></br>
       <br></br>
       <br></br>
-      <h2>CHECK OUT MY PROJECTS!</h2>
-      <br></br>
-      <a href="/projects" className="button">Explore Projects</a>
+      <div id="projects">
+        <br></br>
+        <h1>PROJECTS</h1>
+        <h2>Click a tab to learn more!</h2>
+        <ProjectCards />
+        {/* <a href="/projects" className="button">Explore Projects</a> */}
+      </div>
       <br></br>
       <br></br>
       <br></br>
       
       <div className="info-card contact-card" id="contact">
-        <div className="info-card-text contact-card-text">
-          <br></br>
+        <div className="info-card-text contact-card-text contact-left">
+          <h1>CONTACT ME</h1>
           <h2>SHOOT ME AN EMAIL!</h2>
-          <br></br>
-          <a href="mailto:aarnapatel18@gmail.com" className="button contactbutton">Send Email</a>
-          <br></br>
-          <br></br>
+          <a href="mailto:aarnapatel18@gmail.com" className="button contactbutton">SEND EMAIL</a>
+        </div>
+
+        <div className="info-card-text contact-card-text contact-right">
           <h2>OR SEND ME A MESSAGE!</h2>
           <ContactForm />
-          <br></br>
         </div>
       </div>
 
